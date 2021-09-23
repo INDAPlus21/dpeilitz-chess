b432babbe71d76a397fed89456b0b608ab14085c
dpeilitz <dpeilitz@kth.se>
2021-09-24 01:48:10 +0200

    Slight progress

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@ use std::fmt;
 pub enum GameState {
     InProgress,
     Check,
-    GameOver
+    GameOver,
 }
 
 /* IMPORTANT:
@@ -13,63 +13,112 @@ pub enum GameState {
  */
 
 #[derive(Copy, Clone, Debug, PartialEq)]
-pub enum Colour{
-    White, Black
+pub enum Colour {
+    White,
+    Black,
 }
 
 #[derive(Copy, Clone, Debug, PartialEq)]
-pub enum Piece{
-    King, Queen, Rook, Knight, Bishop, Peasant 
+pub enum Piece {
+    King,
+    Queen,
+    Rook,
+    Knight,
+    Bishop,
+    Peasant,
 }
 
 pub struct Game {
     /* save board, active colour, ... */
     board: [[Option<(Piece, Colour)>; 8]; 8],
     active_colour: Colour,
-    state: GameState
+    state: GameState,
 }
 
 impl Game {
     /// Initialises a new board with pieces.
     pub fn new() -> Self {
         //Use all types in Piece and Colour
-        use Piece::*;
         use Colour::*;
+        use Piece::*;
         Self {
             /* initialise board, set active colour to white, ... */
             board: [
-                [Some((Bishop, White)), Some((Knight, White)), Some((Rook, White)), Some((Queen, White)), Some((King, White)), Some((Knight, White)), Some((Rook, White)), Some((Bishop, White))],
-                [Some((Peasant, White)), Some((Peasant, White)), Some((Peasant, White)), Some((Peasant, White)), Some((Peasant, White)), Some((Peasant, White)), Some((Peasant, White)), Some((Peasant, White))],
+                [
+                    Some((Bishop, White)),
+                    Some((Knight, White)),
+                    Some((Rook, White)),
+                    Some((Queen, White)),
+                    Some((King, White)),
+                    Some((Knight, White)),
+                    Some((Rook, White)),
+                    Some((Bishop, White)),
+                ],
+                [
+                    Some((Peasant, White)),
+                    Some((Peasant, White)),
+                    Some((Peasant, White)),
+                    Some((Peasant, White)),
+                    Some((Peasant, White)),
+                    Some((Peasant, White)),
+                    Some((Peasant, White)),
+                    Some((Peasant, White)),
+                ],
                 [None; 8],
                 [None; 8],
-                [None; 8], 
                 [None; 8],
-                [Some((Peasant, Black)), Some((Peasant, Black)), Some((Peasant, Black)), Some((Peasant, Black)), Some((Peasant, Black)), Some((Peasant, Black)), Some((Peasant, Black)), Some((Peasant, Black))],
-                [Some((Bishop, Black)), Some((Knight, Black)), Some((Rook, Black)), Some((Queen, Black)), Some((King, Black)), Some((Knight, Black)), Some((Rook, Black)), Some((Bishop, Black))]],
+                [None; 8],
+                [
+                    Some((Peasant, Black)),
+                    Some((Peasant, Black)),
+                    Some((Peasant, Black)),
+                    Some((Peasant, Black)),
+                    Some((Peasant, Black)),
+                    Some((Peasant, Black)),
+                    Some((Peasant, Black)),
+                    Some((Peasant, Black)),
+                ],
+                [
+                    Some((Bishop, Black)),
+                    Some((Knight, Black)),
+                    Some((Rook, Black)),
+                    Some((Queen, Black)),
+                    Some((King, Black)),
+                    Some((Knight, Black)),
+                    Some((Rook, Black)),
+                    Some((Bishop, Black)),
+                ],
+            ],
             active_colour: White,
-            state: GameState::InProgress
-
+            state: GameState::InProgress,
         }
     }
 
-    /// If the current game state is InProgress and the move is legal, 
+    /// If the current game state is InProgress and the move is legal,
     /// move a piece and return the resulting state of the game.
     pub fn make_move(&mut self, _from: String, _to: String) -> Option<GameState> {
         //check if inProgress
-        if self.get_game_state() == GameState::InProgress{
-
-        }
-        //Find what piece is on tile
-        //find if move is legal
+        if self.get_game_state() == GameState::InProgress {}
+        let piece = self.what_is_on(&_from);
+        if self.is_legal(piece, _from, _to) == true {};
+        //Find what piece is on tile DONE
+        //find if move is legal DOING
         //move piece
         //check if Gamestate has changed
         //return GameState
         None
     }
-    pub fn what_is_on(&self, _tile: String) -> Option<(Piece, Colour)> {
-        let row: &char = &_tile[..1].parse().ok().unwrap();
-        let column: &usize = &_tile[1..].parse().ok().unwrap();
-        let row = match row {
+
+    pub fn what_is_on(&self, _tile: &String) -> Option<(Piece, Colour)> {
+        //Get index for position and return whatever is on that index
+        let tile = self.get_index(_tile);
+        self.board[tile.0][tile.1]
+    }
+    pub fn get_index(&self, _tile: &String) -> (usize, usize) {
+        //split string and turn into seperate variables
+        let row: char = _tile[..1].parse().ok().unwrap();
+        let column: usize = _tile[1..].parse().ok().unwrap();
+        let row: usize = match row {
             'A' => 0,
             'B' => 1,
             'C' => 2,
@@ -79,15 +128,41 @@ impl Game {
             'G' => 6,
             'H' => 7,
             //if it is outside of the scope it's always mapped to 0
-            _ => 0
-            };
-            //search position by index and check the occupance
-        self.get_tile(&row, &column)
+            _ => 0,
+        };
+        (row, column)
+    }
 
+    pub fn is_legal(&self, _piece: Option<(Piece, Colour)>, _from: String, _to: String) -> bool {
+        use Piece::*;
+        match _piece.unwrap().0 {
+            King => self.king_moves(_from, _to),
+            Queen => self.queen_moves(_from, _to),
+            Knight => self.knight_moves(_from, _to),
+            Rook => self.rook_moves(_from, _to),
+            Bishop => self.bishop_moves(_from, _to),
+            Peasant => self.peasant_moves(_from, _to),
+        }
     }
-    pub fn get_tile(& self, &row: &usize, &column: &usize) -> Option<(Piece, Colour)>{
-        self.board[row][column]
+    pub fn king_moves(&self, _from: String, _to: String) -> bool {
+        false
     }
+    pub fn queen_moves(&self, _from: String, _to: String) -> bool {
+        false
+    }
+    pub fn knight_moves(&self, _from: String, _to: String) -> bool {
+        false
+    }
+    pub fn rook_moves(&self, _from: String, _to: String) -> bool {
+        false
+    }
+    pub fn bishop_moves(&self, _from: String, _to: String) -> bool {
+        false
+    }
+    pub fn peasant_moves(&self, _from: String, _to: String) -> bool {
+        false
+    }
+
     /// Set the piece type that a peasant becames following a promotion.
     pub fn set_promotion(&mut self, _piece: String) -> () {
         //find the piece that is to be upgraded by looping through the top row
@@ -100,10 +175,10 @@ impl Game {
     pub fn get_game_state(&self) -> GameState {
         self.state
     }
-    
-    /// If a piece is standing on the given tile, return all possible 
-    /// new positions of that piece. Don't forget to the rules for check. 
-    /// 
+
+    /// If a piece is standing on the given tile, return all possible
+    /// new positions of that piece. Don't forget to the rules for check.
+    ///
     /// (optional) Don't forget to include en passent and castling.
     pub fn get_possible_moves(&self, _postion: String) -> Option<Vec<String>> {
         //find out what piece is on the given tile
@@ -113,9 +188,8 @@ impl Game {
 
     //
 }
-
 /// Implement print routine for Game.
-/// 
+///
 /// Output example:
 /// |:----------------------:|
 /// | R  Kn B  K  Q  B  Kn R |
@@ -130,7 +204,6 @@ impl Game {
 impl fmt::Debug for Game {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         /* build board representation string */
-        
         write!(f, "")
     }
 }
@@ -154,7 +227,6 @@ mod tests {
     // check that game state is in progress after initialisation
     #[test]
     fn game_in_progress_after_init() {
-
         let game = Game::new();
 
         println!("{:?}", game);
